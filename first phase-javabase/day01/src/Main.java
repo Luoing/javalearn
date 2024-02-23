0cc87824a1fc07957c68e3f90d0b403d573ebccb
luo <284257802@qq.com>
2024-02-23 15:28:06 +0800

    Initial commit

---
+++ first phase-javabase/day01/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+        float t1= 123.4f;
+        System.out.println(0.1+0.2);
+    }
+
+}
